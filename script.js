4737d574e29c631f19b7857ad5217c187af9619f
kokjp1 <james.kok@hva.nl>
2024-03-24 13:28:45 +0100

    JS netjes overzichtelijk maken

--- script.js
+++ script.js
@@ -1,146 +1,189 @@
-let input = document.getElementById('spelerTimeInput');
-console.log(input);
+//variabele declaraties ----------------------------------------
+const timerDisplay = document.getElementById("timer");
+const scoreDisplay = document.getElementById("score");
+const game = document.getElementById("game");
+const startButton = document.getElementById("start");
+const pregamestartbutton = document.getElementById("pregamestart");
+let input = document.getElementById("spelerTimeInput");
+    console.log(input);
+
 let time;
-const timerDisplay = document.getElementById('timer');
 let timer;
 let berrySpawnInterval;
+
 const aantalGaatjes = 16;
 let score = 0;
-const scoreDisplay = document.getElementById('score');
-const game = document.getElementById('game');
-const startButton = document.getElementById('start');
 let gameStarted = false;
+let result = document.getElementById("result");
 
-const berryhitaudio = new Audio('audio/berryclick.mp3');
-const berryslapaudio = new Audio('audio/berryslap.mp3');
-const gameoveraudio = new Audio('audio/gameover.mp3');
-const backgroundmusic = new Audio('audio/backgroundmusic.mp3');
-backgroundmusic.volume = 0.05;
+const berryhitaudio = new Audio("audio/berryclick.mp3");
+const berryslapaudio = new Audio("audio/berryslap.mp3");
+const gameoveraudio = new Audio("audio/gameover.mp3");
+const backgroundmusic = new Audio("audio/backgroundmusic.mp3");
+    backgroundmusic.volume = 0.05;
+//--------------------------------------------------------------
 
+
+//validateinput functie ----------------------------------------
 function validateInput() {
-    let input = document.getElementById('spelerTimeInput');
-    if (input.value < 1 || input.value > 60) {
-        alert('Time must be between 1 and 60');
-        return false;
-    }
-    return true;
+  if (input.value < 1 || input.value > 60) {
+    alert("Time must be between 1 and 60");
+    return false;
+  }
+  return true;
 }
+//--------------------------------------------------------------
 
-const pregamestartbutton = document.getElementById('pregamestart');
+
+//index.html game setup functie --------------------------------
 if (pregamestartbutton) {
-    pregamestartbutton.addEventListener('click', function() {
-        if (validateInput()) {``
-            let input = document.getElementById('spelerTimeInput');
-            localStorage.setItem('inputValue', input.value); //copilot line 
-            window.location.href = 'game.html';
-        }
-    });
+  pregamestartbutton.addEventListener("click", function () {
+    if (validateInput()) {
+      localStorage.setItem("inputValue", input.value); //copilot line
+      window.location.href = "game.html";
+    }
+  });
 }
+//--------------------------------------------------------------
 
+
+//timerfunctie -------------------------------------------------
 function updateTimer() {
-    console.log('Updating timer', timerDisplay, time); 
-    timerDisplay.textContent = time;
-    if (time < 1) {
-        endGame();
-    } else {
-        time--;
-    }
+  console.log("Updating timer", timerDisplay, time);
+  timerDisplay.textContent = time;
+  if (time < 1) {
+    endGame();
+  } else {
+    time--;
+  }
 }
+//---------------------------------------------------------------
 
-window.onload = function() {
-    if (window.location.href.indexOf('game.html') > -1) {
-        time = parseInt(localStorage.getItem('inputValue'), 10);
-        updateTimer();
-    }
-}
 
-for (let i = 0; i < aantalGaatjes; i++) {
-    const hole = document.createElement('div');
-    hole.classList.add('hole');
-    hole.id = `hole${i}`;
-    game.appendChild(hole);
-
-    const mole = document.createElement('div');
-    mole.classList.add('mole');
-    hole.appendChild(mole); 
-
-    mole.addEventListener('click', whack); 
-}
-
-function randomHole() {
-    const gekozengaatje = Math.floor(Math.random() * aantalGaatjes);
-    return document.getElementById(`hole${gekozengaatje}`);
-}
-
-function startGame() {
-    if (gameStarted) {
-        return;
-    }
-    gameStarted = true;
-    backgroundmusic.play();
-    result.style.visibility = 'hidden'; 
-    clearInterval(timer);
-    time = parseInt(localStorage.getItem('inputValue'), 10);
-    
+//multi-html value transfer functie -----------------------------
+function inputvaluetransfer() {
+  if (window.location.href.indexOf("game.html") > -1) {
+    time = parseInt(localStorage.getItem("inputValue"), 10);
     updateTimer();
-    timer = setInterval(updateTimer, 1000);
-    clearInterval(berrySpawnInterval);
+  }
+};
+window.onload = inputvaluetransfer;
+//---------------------------------------------------------------
 
-    berrySpawnInterval = setInterval(function() {
-        const moles = document.querySelectorAll('.mole');
-        moles.forEach(mole => {
-            mole.style.visibility = 'hidden';
-            mole.classList.remove('specialMole'); 
-            mole.classList.remove('peek');
-        });
 
-        mole = moles[Math.floor(Math.random() * moles.length)];
+//gameboard creationfunctie -------------------------------------
+for (let i = 0; i < aantalGaatjes; i++) {
+  const hole = document.createElement("div");
+  hole.classList.add("hole");
+  hole.id = `hole${i}`;
+  game.appendChild(hole);
 
-        if (Math.random() < 0.1) { 
-            mole.classList.add('specialMole');
-        }
-        mole.classList.add('peek');
-        mole.style.visibility = 'visible';}, 600); // berryspawninterval
+  const berry = document.createElement("div");
+  berry.classList.add("berry");
+  hole.appendChild(berry);
+
+  berry.addEventListener("click", whack);
 }
+//---------------------------------------------------------------
 
+
+//randomhole functie --------------------------------------------
+function randomHole() {
+  const gekozengaatje = Math.floor(Math.random() * aantalGaatjes);
+  return document.getElementById(`hole${gekozengaatje}`);
+}
+//---------------------------------------------------------------
+
+
+//berry spawn functie -------------------------------------------
+function spawnBerry() {
+  const berrys = document.querySelectorAll(".berry");
+  berrys.forEach((berry) => {
+    berry.style.visibility = "hidden";
+    berry.classList.remove("avatarBerry");
+    berry.classList.remove("peek");
+  });
+
+  berry = berrys[Math.floor(Math.random() * berrys.length)];
+
+  if (Math.random() < 0.1) {
+    berry.classList.add("avatarBerry");
+  }
+  berry.classList.add("peek");
+  berry.style.visibility = "visible";
+}
+//---------------------------------------------------------------
+
+
+//startgame functie ---------------------------------------------
+function startGame() {
+  if (gameStarted) {
+    return;
+  }
+  gameStarted = true;
+  backgroundmusic.play();
+  result.style.visibility = "hidden";
+  clearInterval(timer);
+  time = parseInt(localStorage.getItem("inputValue"), 10);
+
+  updateTimer();
+  timer = setInterval(updateTimer, 1000);
+  clearInterval(berrySpawnInterval);
+
+  // Use setInterval to call the spawnBerry function every 1000 milliseconds
+  berrySpawnInterval = setInterval(spawnBerry, 1000);
+}
+//---------------------------------------------------------------
+
+
+//endgame functie -----------------------------------------------
 function endGame() {
-    gameStarted = false;
-    backgroundmusic.pause();
-    backgroundmusic.currentTime = 0; //backgroundmusic opgezocht https://stackoverflow.com/questions/17636310/play-audio-and-restart-it-onclick
-    clearInterval(berrySpawnInterval); 
-    clearInterval(timer);
-    timerDisplay.textContent = "Game Over!";
-    gameoveraudio.play();
+  gameStarted = false;
+  backgroundmusic.pause();
+  backgroundmusic.currentTime = 0; //backgroundmusic opgezocht https://stackoverflow.com/questions/17636310/play-audio-and-restart-it-onclick
+  clearInterval(berrySpawnInterval);
+  clearInterval(timer);
+  timerDisplay.textContent = "Game Over Beertje!";
+  gameoveraudio.play();
 
-    const moles = document.querySelectorAll('.mole');
-    moles.forEach(function(mole) {
-        mole.style.visibility = 'hidden';
-        mole.classList.remove('specialMole'); 
-    });
-	let result = document.getElementById('result'); 
-	result.textContent = "Time is up!";
-	result.style.visibility = 'visible';
+  const berrys = document.querySelectorAll(".berry");
+  berrys.forEach(function (berry) {
+    berry.style.visibility = "hidden";
+    berry.classList.remove("avatarBerry");
+  });
+  let result = document.getElementById("result");
+  result.textContent = "Time is up!";
+  result.style.visibility = "visible";
 }
+//---------------------------------------------------------------
 
+
+//whack functie -------------------------------------------------
 function whack(e) {
-    console.log('Hit!', e);
-    score++; 
-    berryslapaudio.play();
-    if (e.target.classList.contains('specialMole')) {
-        score += 2; 
-        berryhitaudio.play();
-    }
+  console.log("Hit!", e);
+  score++;
+  berryslapaudio.play();
+  if (e.target.classList.contains("avatarBerry")) {
+    score += 2;
+    berryhitaudio.play();
+  }
 
-    scoreDisplay.textContent = `Score: ${score}`; 
-    e.target.style.visibility = 'hidden'; 
+  scoreDisplay.textContent = `Score: ${score}`;
+  e.target.style.visibility = "hidden";
+  e.target.classList.remove("peek");
 }
 
-startButton.addEventListener('click', function() {
-    score = 0; 
-    scoreDisplay.textContent = `Score: ${score}`; 
-    startGame(); 
+startButton.addEventListener("click", function () {
+  score = 0;
+  scoreDisplay.textContent = `Score: ${score}`;
+  startGame();
 });
+//----------------------------------------------------------------
 
-let result = document.getElementById('result'); 
+//uitleg aan copilot gevraagd over; Template literals, hele classlist principe, audio en localstorage functies,
 
-//issue dat de tijd pas update als ik de game start 
+//Als ik er nog langer aan kon doorwerken:
+  // - Een highscore/personal record systeem
+  // - on hit animaties 
+  // - animaties, etc perfectioneren
+  // - misschien meer instelbare factoren maken zoals hoe snel berry spawnt en misschien zelf kiezen welke geluiden afspelen
